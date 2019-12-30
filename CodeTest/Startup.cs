caf2597c0315a9b2ae3d8fb93d4a485da7b13934
molham <molham225@gmail.com>
2019-12-30 09:55:45 +0300

    Removed unused files..

--- CodeTest/Startup.cs
+++ CodeTest/Startup.cs
@@ -16,7 +16,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using MongoDB.GenericRepository.Interfaces;
 
 namespace CodeTest
 {
